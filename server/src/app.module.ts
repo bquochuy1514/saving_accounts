5176e40dcf5f43b5bc55a6022a2c10ab0765c3f0
bquochuy1514 <23520594@gm.uit.edu.vn>
2026-03-07 16:00:05 +0700

    setup prisma ORM

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
